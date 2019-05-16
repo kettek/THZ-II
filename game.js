50170762fb4d87b0133b1efa3fb3b1418db31a3e
kts of kettek (nyaa) <kettek1@kettek.net>
2019-05-15 22:59:22 -0700

    More adjustments -- early AI testing

--- game.js
+++ game.js
@@ -1,23 +1,30 @@
 M=Math,A=Array
-a=99,Z=20,c=9
+a=49,Z=10,c=9
 y=a*a+a
 R=()=>M.random()*y<<0
-L=A(Z).fill(1)
-P=[],H=[]
+L=A(Z).fill(2)
+P=[]
 m=A(y).fill('.')
-S=(p,c)=>(p=p<<0,m[p]?m[p]=c:0)
-B=(p,s,e,r)=>{for(i=s;i<e;i+=0.05)S(p+(M.sin(i)*r<<0)*a+M.cos(i)*r<<0,'#')}
-D=(i,o)=>--H[o]<1?L[o]=0:0
-C=(i,p)=>o=P.findIndex(w=>w==p&&w!=i)>0?D(i,o):m[p]=='.'?P[i]=p:0
+B=(p,s,e,r)=>{for(i=s;i<e;i+=.05)m[p+(M.sin(i)*r<<0)*a+M.cos(i)*r<<0]='#'}
+C=(i,p)=>(o=F(p))&&L[o]?--L[o]:m[p]=='.'?P[i]=p:0
+F=(p)=>P.findIndex(w=>w==p)
+H=(i,p,d)=>{
+h=d%a-p%a,v=d/a-p/a;
+u=(h?a:-a)+(v?-1:1)
+C(i,p-u)
+console.log(h+'v'+v+": "+u)
+return ""
+}
 
-for(z in L)P[z]=R(),H[z]=2,B(R(),1,M.PI*2,5),S(R(),'*')
-H[0]=Z
+for(z in L)P[z]=R(),B(R(),1,M.PI*2,5),m[R()]='*'
+L[0]=c
 
 onkeyup=e=>{
 d=1-e.which%4
-d=d%2?d*a:-d-1
-C(0,P[0]+d)
-r=H[0]+"hp\n"
-for(y=-c;y<c;y++){for(x=-c;x<c;x++){t=P[0]+y*a+x,T=P.findIndex(e=>e==t),T>=0?r+=T:r+=m[t]||' '}r+='\n'}
+C(0,P[0]+(d%2?d*a:-d-1))
+r=L[0]+"hp\n"
+for(y=-c;y<c;y++){for(x=-c;x<c;x++){
+p=P[0]+y*a+x,T=F(p),T>=0?r+=L[T]?T==0?'@':'z'+(H(T,p,P[0])):',':r+=m[p]||' '
+}r+='\n'}
 console.log(r)
 }
