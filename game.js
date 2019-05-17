ed85024becee5e60aa84d593902af5fffb13468c
kts of kettek (nyaa) <kettek1@kettek.net>
2019-05-16 20:43:23 -0700

    536/560: The dark arts have rendered unto me 10 more characters

--- game.js
+++ game.js
@@ -1,20 +1,19 @@
 M=Math,A=Array
 R=_=>M.random()*y<<0
-a=70
-y=a*a+a
+a=80
+y=a*a
 L=A(30).fill(2)
 P=[]
 m=A(y).fill('.')
 B=p=>{for(s=0;s<6;s+=.4)m[p+~~(M.sin(s)*5)*a+M.cos(s)*5<<0]='#'}
-C=(i,p,o=F(p))=>L[o]?--L[o]:m[p]=='.'?P[i]=p:0
+C=(p,o=F(p))=>L[o]?--L[o]:m[p]=='.'?P[i]=p:0
 F=p=>P.findIndex(w=>w==p)
-H=(i,p,d)=>C(i,p+(d%a-p%a>0?1:-1)+(~~d/a-p/a>0?a:-a))
+H=(p,d)=>C(p+(d%a-p%a>0?1:-1)+(~~d/a-p/a>0?a:-a))
 for(z in L)P[z]=R(),B(R()),m[R()]='*'
 m[R()]='H'
 L[0]=c=9
 onkeyup=e=>{d=1-e.which%4
-C(0,P[0]+(d%2?d*a:-d-1))
-for(z in L)L[++z]?H(z,P[z],P[0]):0
+for(i in L)L[i]?~~i?H(P[i],P[0]):C(P[0]+(d%2?d*a:-d-1)):0
 r=''
 for(y=-c;y++<c;){for(x=-c;x++<c;){
 p=P[0]+y*a+x
