201794f601927016efc6e031cd472e39514f1156
kts of kettek <kettek1@kettek.net>
2019-05-18 19:05:54 -0700

    560/560: Give undead some personality
    This has technically taken up two bytes so I have done another bad to gain a character.

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ R=_=>M.random()*s<<0
 L=A(0,c=9,...A(30).fill(2))
 P=[]
 m=A(s=(a=90)*a).fill('.')
-B=p=>{for(b=0;b<6;b+=.4)m[p+~~(M.sin(b)*5)*a+M.cos(b)*5<<0]='#'}
+B=p=>{for(b=0;b<6;b+=.4)m[p+~(M.sin(b)*5)*a+M.cos(b)*5<<0]='#'}
 F=p=>P.findIndex(w=>w==p)
 C=(p,o=F(p))=>L[o]?--L[o]:m[p]=='.'?P[i]=p:m[P[0]]='.'
 H=(p,d)=>C(p+(d%a-p%a>0?1:-1)+(~~d/a-p/a>0?a:-a))
@@ -11,5 +11,5 @@ for(i in L)P[i]=R(),B(R()),m[R()]='*'
 onkeyup=e=>{d=1-e.which%4
 for(i in L)L[i]?i-1?H(P[i],P[1]):C(P[1]+(d%2?d*a:-d-1)):r=''
 for(y=-c;y++<c;r+=`
-`)for(x=-c;x++<c;r+=T+1?T?L[T]?T-1?'z':L[1]:',':'H':m[p]||'~')T=F(p=P[1]+y*a+x)
+`)for(x=-c;x++<c;r+=T+1?T?L[T]?T-1?'ö':L[1]:',':'H':m[p]||'~')T=F(p=P[1]+y*a+x)
 console.log(L[1]?P[0]-P[1]?r:"yeet":"heck")}
